26d484fb5d9efb5d47f69ab8dd74f9f335ac8802
Vadim <vadim.peczynski@gmail.com>
2017-06-05 21:53:17 +0200

    Projekt potrzebny na trzecie warsztaty

---
+++ 3.Nawigacja/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize { get; set; }
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            PageSize = 4;
+        }
+        public ViewResult List(int page=1)
+        {
+            var model = new ProductsListViewModel
+            {
+                Products = _productRepository.Products
+                    .OrderBy(p => p.ProductId).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _productRepository.Products.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ 3.Nawigacja/SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingExtensions
+    {
+        public static HtmlString PageLinks(this IHtmlHelper html,
+                                            PagingInfo pagingInfo,
+                                            Func<int, string> pageUrl)
+        {
+            var result = new StringBuilder();
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml.Append(i.ToString());
+                if (i==pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.GetString());
+            }
+            return new HtmlString(result.ToString());
+        }
+
+        public static string GetString(this IHtmlContent content)
+        {
+            var writer = new System.IO.StringWriter();
+            content.WriteTo(writer, HtmlEncoder.Default);
+            return writer.ToString();
+        }
+    }
+}
